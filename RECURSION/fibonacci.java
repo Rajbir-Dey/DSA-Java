7680b2c8bb8b3097618292fa15096280db055dc8
Rajbirrrr <rajbirdey1@gmail.com>
2026-03-02 17:19:38 +0530

    RECURSION

--- RECURSION/fibonacci.java
+++ RECURSION/fibonacci.java
@@ -1,3 +1,21 @@
 class fibonacci{
     
+    
+    public static int calculatefibonacci(int n){
+        //basecase
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }else{
+            return calculatefibonacci(n-1)+calculatefibonacci(n-2);
+        }
+
+    }
+    
+    public static void main(String args[]){
+        int n = 6;
+        System.out.println(calculatefibonacci(n));
+    }
 }
